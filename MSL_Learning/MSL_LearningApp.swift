3ae668ef81f371dd5da73775000419734722a3eb
Maxnxi <nisq@ya.ru>
2026-01-01 22:36:10 +0700

    Initial Commit

---
+++ MSL_Learning/MSL_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSL_LearningApp.swift
+//  MSL_Learning
+//
+//  Created by Maksim Ponomarev on 1/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MSL_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
